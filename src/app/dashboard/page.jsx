7d67cbc9ac33ad44f2a5bdba9748c86a66cfdf02
Tam Phan <Tam2690@gmail.com>
2023-10-27 23:58:47 +0700

    [CF-14]: Add app/dashboard/shareurl route

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,12 +3,11 @@
 import DashboardSidebar from '@/components/DashboardSidebar';
 import DashboardStyled from './styles';
 import DashboardFavoriteStores from '@/components/DashboardFavoriteStores';
-import DashboardShareUrl from '@/components/DashboardShareUrl';
 
 const Dashboard = () => (
   <DashboardStyled>
     <DashboardSidebar />
-    <DashboardShareUrl />
+    <DashboardFavoriteStores />
   </DashboardStyled>
 );
 
