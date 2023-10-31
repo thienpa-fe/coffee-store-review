3d90b8a1f995c1b0678f7952984fc5d80c7bce26
Tam Phan <Tam2690@gmail.com>
2023-10-31 17:50:00 +0700

    [CF-47]: Build Dashboard > Statistic > Tab Coffee Store UI

---
+++ src/components/CoffeeStoreStatistic/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const CoffeeStoreStatisticStyled = styled.div`
+  margin-top: 50px;
+
+  .your-stores-statistic {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 20px;
+  }
+
+  .ant-statistic {
+    text-align: center;
+  }
+`;
+
+export default CoffeeStoreStatisticStyled;
--- src/components/StatisticTabs/index.jsx
+++ src/components/StatisticTabs/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Tabs } from 'antd';
 import StatisticTabsStyled from './styles';
+import CoffeeStoreStatistic from '../CoffeeStoreStatistic';
 
-const StatisticTabs = () => (
-  <StatisticTabsStyled>
-    <Tabs
-      type="card"
-      items={['Coffee Store', 'Reviews', 'Reactions'].map((value, index) => ({
-        label: `${value}`,
-        key: index,
-        children: `${value}`,
-      }))}
-    />
-  </StatisticTabsStyled>
-);
+const StatisticTabs = () => {
+  const statisticTabs = [
+    { tabName: 'Coffee Store', data: <CoffeeStoreStatistic /> },
+    { tabName: 'Reviews', data: 'content reviews' },
+    { tabName: 'Reactions', data: 'content Reactions' },
+  ];
+
+  return (
+    <StatisticTabsStyled>
+      <Tabs
+        type="card"
+        items={statisticTabs.map(({ tabName, data }, index) => ({
+          label: tabName,
+          key: index,
+          children: data,
+        }))}
+      />
+    </StatisticTabsStyled>
+  );
+};
 export default StatisticTabs;
