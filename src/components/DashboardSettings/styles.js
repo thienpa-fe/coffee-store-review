f7a02fc457ac0bc7be2c5c18cad43b984650c808
canhtran <vancanh101194@gmail.com>
2023-10-31 15:45:32 +0700

    [CF-5]: Dashboard-Settings-Theme UI

---
+++ src/components/DashboardSettings/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const DashboardSettingsStyled = styled.div`
+  padding: 30px 60px 30px 280px;
+
+  .ant-form-item {
+    margin-top: 100px;
+  }
+
+  .ant-select-selector {
+    border: 2px solid #808080 !important;
+  }
+
+  @media only screen and (max-width: 375px) {
+    padding-left: 70px;
+    padding-right: 30px;
+  }
+`;
+
+export default DashboardSettingsStyled;
