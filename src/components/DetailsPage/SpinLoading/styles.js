20c9da1701f3e84a4012439cb236783a74cc1d04
canhtran <vancanh101194@gmail.com>
2023-11-10 18:16:49 +0700

    [CF-43]: Create lazy loading UI

---
+++ src/components/DetailsPage/SpinLoading/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const SpinLoadingStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+
+  .content {
+    margin-left: 10px;
+    color: gray;
+  }
+`;
+
+export default SpinLoadingStyled;
