263edb1f9597216d371c1523e6570be77202814a
Tam Phan <tam2690@gmail.com>
2023-11-13 20:52:26 +0700

    [CF-53]: Redirect /dashboard to /dashboard/my-stores

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,8 @@
 'use client';
+import { redirect } from 'next/navigation';
 
-import DashboardMyStoresContainer from '@/components/Dashboard/containers/DashboardMyStoresContainer';
-
-const Dashboard = () => (
-  <>
-    <DashboardMyStoresContainer />
-  </>
-);
+const Dashboard = () => {
+  redirect('/dashboard/my-stores');
+};
 
 export default Dashboard;
