b74f17111bda3df0172784ce67c7783270761892
Tam Phan <tam2690@gmail.com>
2023-11-16 12:18:50 +0700

    [CF-6]: Fix width of Dashboard My Profile container

--- src/components/Dashboard/containers/DashboardMyProfileContainer/index.jsx
+++ src/components/Dashboard/containers/DashboardMyProfileContainer/index.jsx
@@ -1,10 +1,5 @@
 import DashboardMyProfileContainerStyled from '@/components/Dashboard/containers/DashboardMyProfileContainer/styles';
-import { Card } from 'antd';
 import Title from 'antd/es/typography/Title';
-// import ImgCrop from 'antd-img-crop';
-import React, { useState } from 'react';
-import { PlusOutlined } from '@ant-design/icons';
-import { Modal, Upload } from 'antd';
 import UserInformationCard from '@/components/Dashboard/UserInfomationCard';
 import UpdateUserInfoCard from '@/components/Dashboard/UpdateUserInfoCard';
 
--- src/components/Dashboard/containers/DashboardMyProfileContainer/styles.js
+++ src/components/Dashboard/containers/DashboardMyProfileContainer/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const DashboardMyProfileContainerStyled = styled.div`
   padding: 30px;
-  max-width: 800px;
 `;
 
 export default DashboardMyProfileContainerStyled;
