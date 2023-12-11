3892c9453efccd1b1dc6dad3069660af1f3cb31b
Tam Phan <tam2690@gmail.com>
2023-12-11 20:18:13 +0700

    [CF-20]: Homepage > Load random list of coffee stores

--- src/components/HomePage/containers/HomePageCoffeeShopsList/index.jsx
+++ src/components/HomePage/containers/HomePageCoffeeShopsList/index.jsx
@@ -1,42 +1,29 @@
 import HomePageCoffeeShopCard from '@/components/HomePage/HomePageCoffeeShopCard';
 import HomePageCoffeeShopsListStyled from './styles';
 import { Col, Row } from 'antd';
+import { listStoresMock } from '@/data/ListStoresMock';
+import dynamic from 'next/dynamic';
 
-const HomePageCoffeeShopsList = () => (
-  <HomePageCoffeeShopsListStyled>
-    <Row gutter={[32, 32]}>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-      <Col xl={8} lg={12}>
-        <HomePageCoffeeShopCard />
-      </Col>
-    </Row>
-  </HomePageCoffeeShopsListStyled>
-);
+const HomePageCoffeeShopsList = () => {
+  const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
 
-export default HomePageCoffeeShopsList;
+  const shuffledListStores = shuffleArray(listStoresMock);
+
+  return (
+    <HomePageCoffeeShopsListStyled>
+      <Row gutter={[32, 32]}>
+        {shuffledListStores.length
+          ? shuffledListStores.map((store) => (
+              <Col xl={8} lg={12} key={store._id}>
+                <HomePageCoffeeShopCard data={store} />
+              </Col>
+            ))
+          : 'No Data'}
+      </Row>
+    </HomePageCoffeeShopsListStyled>
+  );
+};
+
+export default dynamic(() => Promise.resolve(HomePageCoffeeShopsList), {
+  ssr: false,
+});
